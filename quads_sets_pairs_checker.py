4f36498c0e56a0099b1fbc9a8756566a37a9c2f7
MrClub <40201366+MrClub@users.noreply.github.com>
2020-01-18 09:54:36 +0000

    Added something to recognise if a set, two pair or quads. Also, due to legth of list being four added function to distinguish between 2 pair or quads.

--- quads_sets_pairs_checker.py
+++ quads_sets_pairs_checker.py
@@ -39,11 +39,50 @@ def quads_sets_pairs_check(hero, villian, community):
     print(hero_pair,"\n", villian_pair)
     return hero_pair,villian_pair
 
+def quads_sets_pairs_ranking(heros_quads_sets_pairs,
+                             villians_quads_sets_pairs):
+    # this will rank the quads,sets and pairs.
+    if len(heros_quads_sets_pairs) == 4:
+        #for card in heros_quads_sets_pairs:
+
+        print("Hero has two pair or quads")
+    if len(villians_quads_sets_pairs) == 4:
+        print("Villian has two pair or quads")
+    if len(heros_quads_sets_pairs) == 3:
+        print("Hero has a set")
+    if len(villians_quads_sets_pairs) == 3:
+        print("Villian has a set")
+    if heros_quads_sets_pairs == []:
+        print("Hero has nothing")
+    if villians_quads_sets_pairs == []:
+        print("Villian has nothing")
+    if len(heros_quads_sets_pairs)  == 2:
+        print("hero has a pair")
+    if len(villians_quads_sets_pairs) == 2:
+        print("Villian has a pair")
+
+def quads_or_two_pair_check(hand_to_check):
+    # this will check if the list is quads or two pair. return true for quads, false for two pair
+    count = 0
+    for card in hand_to_check:
+        if card[0] == hand_to_check[0][0]:
+            count +=1
+    if count == 4:
+        print("True")
+        return True
+    else:
+        print("False")
+        return False
+
 test_hero = [[7,"Hearts"],[7,"Clubs"]]
 test_villian = [[8,"Clubs"],[10,"Spades"]]
 test_community = [[3, 'Clubs'], [8, 'Spades'], [6, 'Diamonds'], [8, 'Hearts'], [7, 'Spades']]
 
-quads_sets_pairs_check(heros_hand, villians_hand, community_cards)
+hero_madehand, villian_madehand = quads_sets_pairs_check(heros_hand, villians_hand, community_cards)
+
+quads_sets_pairs_ranking(hero_madehand,villian_madehand)
+
+quads_or_two_pair_check([[10, 'Diamonds'], [10, 'Spades'], [11, 'Spades'], [11, 'Diamonds']])
 
 #quads_sets_pairs_check(test_hero, test_villian, test_community)
 
