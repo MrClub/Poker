199ea51bb4e333cd84e42a5c32f567ab6e1419f4
MrClub <40201366+MrClub@users.noreply.github.com>
2020-03-03 12:06:10 +0000

    Created straigt_flush_check , but need to think on how to implement it

---
+++ straight_flush_check.py
@@ -0,0 +1,6 @@
+# This will take a confirmed straight and a confirmed flush and compare them to see if they combine to be a straight flush.
+
+import flush_check as flush
+import straight_check as straigh
+
+def straight_flush_check(confirmed_straight, confirmed_flush)
