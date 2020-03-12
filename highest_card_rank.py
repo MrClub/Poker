03ebfd59f5590da85abfe22758b781a0383030e0
MrClub <40201366+MrClub@users.noreply.github.com>
2020-03-12 21:10:18 +0000

    -Deleted straight_flush_check.py, highest_card_rank.py and quads_sets_pairs_checker.py. They are in hand_rank.py now.

--- highest_card_rank.py
+++
@@ -1,5 +0,0 @@
-def high_card(hole_cards, community_cards):
-    high_card_hand = sorted(hole_cards + community_cards, reverse=True)
-    high_card_hand.pop()
-    high_card_hand.pop()
-    return high_card_hand, f"High Card {high_card_hand[0][0]}"
